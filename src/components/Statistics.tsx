d04ad48aabec079c18116b13a936f946426a716c
Mallina Dhanu <mallinadhanu@gmail.com>
2026-05-15 18:44:13 +0530

    fix: Update CountUp import to default export

--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { motion } from 'framer-motion'
-import { CountUp } from 'react-countup'
+import  CountUp  from 'react-countup'
 import { useInView } from 'react-intersection-observer'
 
 export function Statistics() {
